1eb0924de15e0853c6ddb8b873a3554e5ad7af6b
Jeremy E <34139712+JeremyEastham@users.noreply.github.com>
2022-09-30 18:23:01 -0500

    Fix frame imports

---
+++ agtern/gui/frames/__init__.py
@@ -0,0 +1,5 @@
+from .internship_detail_frame import InternshipDetailFrame
+from .internship_list_frame import InternshipListFrame
+from .profile_frame import ProfileFrame
+from .top_bar_frame import TopBarFrame
+from .vertical_scrolled_frame import VerticalScrolledFrame
