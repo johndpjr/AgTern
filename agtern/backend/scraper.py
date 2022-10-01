510e891d80e596dcba162452d0386477cca300a0
Jeremy E <34139712+JeremyEastham@users.noreply.github.com>
2022-09-30 22:57:15 -0500

    Implement scraping algorithm!

--- agtern/backend/scraper.py
+++ agtern/backend/scraper.py
@@ -13,43 +13,70 @@ from selenium.webdriver.support.wait import WebDriverWait
 import selenium.webdriver.support.expected_conditions as condition
 from selenium.webdriver.common.by import By
 
-from agtern.core import Internship
+import signal
 
-def scrape():
+from agtern.models import Internship
+
+def scrape( headless: bool = True ):
     """Pre-MVP: This function scrapes all websites in the config and stores them in a file.
     Post-MVP: This function will take arguments to specify how and where to scrape. The results will be stored in a database."""
     driver = None
+
+    def close_driver( signal_number = None, frame = None ):
+        if driver is not None:
+            driver.close()
+        exit( 0 )
+
+    # Ensure driver is closed if interrupted:
+    # signal.signal( signal.SIGINT, close_driver )
+
     try:
         options = Options()
-        # options.headless = True
+        options.headless = headless
         driver = webdriver.Chrome( options = options )
         wait = WebDriverWait( driver, 5 )
-        driver.get( "https://www.tesla.com/careers/search/?query=Internship&site=US" )
-        tbody: WebElement = wait.until(
-            condition.presence_of_element_located( ( By.CLASS_NAME, "tds-table-body" ) )
-        )
+
+        with open( "scraping_config.json", "r" ) as f:
+            config = json.load( f )
+
         internships = []
-        for tr in tbody.find_elements( By.CLASS_NAME, "tds-table-row" ):
-            res = tr.find_elements( By.TAG_NAME, "td" )
-            info = res[0].find_element( By.TAG_NAME, "a" )
-            title = info.text
-            link = info.get_attribute( "href" )
-            category = res[1].text
-            location = res[2].text
-            internship = Internship( title, link, category, location )
-            internships.append( dataclasses.asdict( internship ) )
-            print( f"{internship}\n" )
+        for company_config_entry in config:
+            company_name = company_config_entry["company"]
+            if "scrape" not in company_config_entry.keys() or len( company_config_entry["scrape"].keys() ) == 0:
+                print( f"{company_name} does not have any scrapeable properties. Skipping!" )
+                continue
+
+            print( f"Scraping {company_name}..." ) # Company name
+
+            driver.get( company_config_entry["link"] )
+
+            data = {}
+            for property in company_config_entry["scrape"].keys():
+                elements = wait.until(
+                        condition.presence_of_all_elements_located(
+                                ( By.XPATH, company_config_entry["scrape"][property] )
+                        )
+                )
+                data[property] = [ element.text for element in elements ]
+
+            for i in range( len( data[list( data.keys() )[0]] ) ):
+                internship = {
+                    "company": company_name
+                }
+                for property in data.keys():
+                    internship[property] = data[property][i]
+                internships.append( internship )
+
         print( "Writing to database..." )
-        with open( "db.json", "w" ) as db:
-            db.write( json.dumps( internships ) )
+        with open( "db.json", "w" ) as f:
+            json.dump( internships, f, indent = 2 )
         print( "Done!" )
     finally:
         # Ensure driver is closed if an exception occurs
-        if driver is not None:
-            driver.close()
+        close_driver()
 
 def start_scraper():
     open( "db.json", "w+" ) # Create json file if it doesn't exist
-    scraper = Process( target = scrape )
+    scraper = Process( target = scrape, args = ( False, ) ) # DO NOT REMOVE COMMA!!
     scraper.daemon = True # Run in background, so it doesn't block the GUI (if shown)
     scraper.start()
