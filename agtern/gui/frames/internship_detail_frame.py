ce56ebc979791ff9960e9f8cb01429618f817512
johndpjr <johndpowell02@gmail.com>
2022-09-30 21:55:52 -0500

    remove var placeholders & disable Text box

--- agtern/gui/frames/internship_detail_frame.py
+++ agtern/gui/frames/internship_detail_frame.py
@@ -19,27 +19,30 @@ class InternshipDetailFrame(tk.Frame):
         # Company (e.g. Google)
         self._lbl_internship_company = tk.Label(
             self._lblfrm_internship_details,
-            textvariable=tk.StringVar(parent, value='Company placeholder', name=appvars.INTERNSHIP_COMPANY)
+            textvariable=tk.StringVar(parent, name=appvars.INTERNSHIP_COMPANY)
         )
         # Title (e.g. Software Engineering Intern)
         self._lbl_internship_title = tk.Label(
             self._lblfrm_internship_details,
-            textvariable=tk.StringVar(parent, value='Title placeholder', name=appvars.INTERNSHIP_TITLE)
+            textvariable=tk.StringVar(parent, name=appvars.INTERNSHIP_TITLE)
         )
-        # Year (e.g. 2023)
-        self._var_internship_year = tk.IntVar(self, value=2023)
-        self._lbl_internship_year = tk.Label(
-            self._lblfrm_internship_details,
-            textvariable=tk.StringVar(parent, value='Year placeholder', name=appvars.INTERNSHIP_YEAR)
+        # Container for Period and Year (e.g. Summer 2023)
+        self._frm_internship_period_year = tk.Frame(
+            self._lblfrm_internship_details
         )
         # Period (e.g. Summer)
         self._lbl_internship_period = tk.Label(
-            self._lblfrm_internship_details,
-            textvariable=tk.StringVar(parent, value='Period placeholder', name=appvars.INTERNSHIP_PERIOD)
+            self._frm_internship_period_year,
+            textvariable=tk.StringVar(parent, name=appvars.INTERNSHIP_PERIOD)
+        )
+        # Year (e.g. 2023)
+        self._lbl_internship_year = tk.Label(
+            self._frm_internship_period_year,
+            textvariable=tk.StringVar(parent, name=appvars.INTERNSHIP_YEAR)
         )
         
         # "Apply now" button
-        tk.StringVar(parent, name='internship.link')
+        self._var_internship_link = tk.StringVar(parent, name=appvars.INTERNSHIP_LINK)
         self._bttn_apply_now = tk.Button(
             self,
             text='Apply now',
@@ -50,18 +53,20 @@ class InternshipDetailFrame(tk.Frame):
         self._var_internship_txtbox = tk.StringVar(self)
         self._txtbx_internship_description = tk.Text(
             self,
-            wrap=tk.WORD
+            wrap=tk.WORD,
+            state=tk.DISABLED
         )
 
-        self._lblfrm_internship_details.grid(row=0, column=0, sticky=tk.EW)
-        self._lbl_internship_company.pack(side=tk.TOP, anchor=tk.NW)
-        self._lbl_internship_title.pack(side=tk.TOP, anchor=tk.NW)
-        self._lbl_internship_year.pack(side=tk.TOP, anchor=tk.NW)
-        self._lbl_internship_period.pack(side=tk.TOP, anchor=tk.NW)
+        self._lblfrm_internship_details.grid(row=0, sticky=tk.EW)
+        self._lbl_internship_company.grid(row=0, sticky=tk.W)
+        self._lbl_internship_title.grid(row=1, sticky=tk.W)
+        self._frm_internship_period_year.grid(row=2, sticky=tk.W)
+        self._lbl_internship_period.pack(side=tk.LEFT)
+        self._lbl_internship_year.pack(side=tk.LEFT)
 
-        self._bttn_apply_now.grid(row=1, column=0, sticky=tk.W, pady=3)
+        self._bttn_apply_now.grid(row=1, sticky=tk.W, pady=3)
 
-        self._txtbx_internship_description.grid(row=2, column=0, sticky=tk.NSEW)
+        self._txtbx_internship_description.grid(row=2, sticky=tk.NSEW)
 
         self.grid_rowconfigure(2, weight=1)     # allow row 2 to expand vertically
         self.grid_columnconfigure(0, weight=1)  # allow column 0 to expand horizontally
@@ -70,9 +75,7 @@ class InternshipDetailFrame(tk.Frame):
         """Open url of internship when the "Apply now"
         button is clicked."""
 
-        print('"Apply now" was clicked and the link is ' \
-              f'"{self.getvar("internship.link")}"')
-
-        link = self.getvar('internship.link')
+        link = self.getvar(appvars.INTERNSHIP_LINK)
+        print(f'"Apply now" was clicked and the link is {link}')
         if link:  # Ensure not empty 
             webbrowser.open_new_tab(link)
