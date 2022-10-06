bf8905ae63a197fb9db6cb86d9e3b3e83abc20a1
Brendan Ehrman <brendan.ehrman.1@tamu.edu>
2022-10-05 19:02:20 -0500

    Simplify CSV and JSON generation using Pandas

--- agtern/scripts/import_companies.py
+++ agtern/scripts/import_companies.py
@@ -1,39 +1,40 @@
-
-from agtern.data import DataFile
-import csv
+import pandas as pd
 import json
+from agtern.data import DataFile
 
 
 def import_companies():
     """Imports all company names and links from companies.csv into scraping_config.json."""
 
-    companies_csv = DataFile("companies.csv")
-
-    if not companies_csv.exists():
-        raise FileNotFoundError("companies.csv does not exist!")
-
+    # Gets paths of companies CSV and scraping config JSON
+    companies_csv = DataFile("companies.csv", default_data="name,link")
     scraping_config_json = DataFile("scraping_config.json", default_data="[]")
 
-    original_config = []
+    # Converts readable JSON into object read in by DataFrame
     with open(scraping_config_json.path, "r") as f:
-        original_config = json.load(f)
+        readable_json = json.load(f)
 
-    data = []
-    with open(companies_csv.path, "r") as f:
-        reader = csv.DictReader(f, fieldnames=["company", "link"])
-        for entry in reader:
-            found_existing_entry = False
-            for original_entry in original_config:  # Just update link if entry already exists
-                if original_entry["company"] == entry["company"]:
-                    original_entry["link"] = entry["link"].strip().strip("\"")
-                    data.append(original_entry)
-                    found_existing_entry = True
-            if not found_existing_entry:
-                data.append({
-                    "company": entry["company"],
-                    "link": entry["link"].strip().strip("\""),
-                    "scrape": {}
-                })
+    # Reads in and converts CSV/JSON into Pandas DataFrames
+    company_df = pd.read_csv(companies_csv.path)
+    original_df = pd.DataFrame(readable_json)
 
+    # Creates new JSON object with companies from CSV
+    print("INFO: Generating company info...")
+    new_df = pd.DataFrame()
+    new_df["name"] = company_df["name"]
+    new_df["link"] = company_df["link"]
+    new_df["scrape"] = "{}"
+
+    # Tries to set existing scrape configs to new JSON object.
+    # Keeps default scrape '{}' for all internships if scraping_config.json
+    # 'name' and 'scrape' do not exist (will occur if the file hasn't been created)
+    try:
+        new_df.loc[new_df["name"].isin(original_df["name"]), "scrape"] = \
+            original_df.loc[original_df["name"] == new_df["name"], "scrape"]
+    except:
+        print("WARNING: Setting scrape configurations to '{}'")
+
+    # Rewrites JSON file using new DataFrame (in readable format)
+    print("INFO: Writing info to scraping_config.json...")
     with open(scraping_config_json.path, "w") as f:
-        json.dump(data, f, indent=2)
+        json.dump(readable_json, f, indent=2)
--- agtern/scripts/sort_companies.py
+++ agtern/scripts/sort_companies.py
@@ -1,69 +1,24 @@
-
-import json
+import pandas as pd
+import csv
 from agtern.data import DataFile
 
 
 def sort_companies():
     """Sorts both companies.csv and scraping_config.json by company name."""
+
+    # Create or read in companies.csv
     companies_csv = DataFile(
-        "companies.csv", default_data="")  # Create if it doesn't exist
+        "companies.csv", default_data="name,link")
 
-    # Read the csv
-    with open(companies_csv.path, "r") as f:
-        data = f.read()
+    # Initialize company info containers
+    company_df = pd.read_csv(companies_csv.path)
 
-    # Map name -> line of csv
-    companies = {}
+    # Validate CSV contents if manually given
+    assert (company_df["name"].is_unique)
 
-    # List of names
-    company_names = []
+    # Reformat CSV
+    company_df["link"] = company_df["link"].str.strip()
+    company_df = company_df.sort_values("name")
 
-    # Load and process csv data
-    for line in data.split("\n"):
-        if "," not in line:
-            continue
-        company_name = line.split(",")[0]
-        company_names.append(company_name)
-        companies[company_name] = line
-
-    # Sort the names
-    company_names = sorted(company_names)
-
-    # Construct new csv
-    final_entries = []
-    for company_name in company_names:
-        final_entries.append(companies[company_name])
-
-    # Write the csv
-    with open(companies_csv.path, "w") as f:
-        f.write("\n".join(final_entries))
-
-    scraping_config_json = DataFile("scraping_config.json", default_data="[]")
-
-    # Read the json
-    with open(scraping_config_json.path, "r") as f:
-        data = json.load(f)
-
-    # Map name -> entry of json
-    companies = {}
-
-    # List of names
-    company_names = []
-
-    # Load and process json data
-    for entry in data:
-        company_name = entry["company"]
-        company_names.append(company_name)
-        companies[company_name] = entry
-
-    # Sort the names
-    company_names = sorted(company_names)
-
-    # Construct new json
-    final_entries = []
-    for company_name in company_names:
-        final_entries.append(companies[company_name])
-
-    # Write the json
-    with open(scraping_config_json.path, "w") as f:
-        json.dump(final_entries, f, indent=2)
+    # Write back data sorted by company name
+    company_df.to_csv(companies_csv.path, index=False, quoting=csv.QUOTE_ALL)
