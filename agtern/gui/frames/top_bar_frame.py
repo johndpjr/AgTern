0ca0075fd6fa4f5ceb8a6ed9f3b30876e47dfcb7
John Powell <johndpowell02@gmail.com>
2022-10-06 18:42:39 -0500

    Scrape/33/scrape tesla links (#35)
    * scraper: get links from Tesla
    
    * #15: expand labelframe and text widgets
    
    * #28: remove all refs to "My Internships"

--- agtern/gui/frames/top_bar_frame.py
+++ agtern/gui/frames/top_bar_frame.py
@@ -14,11 +14,6 @@ class TopBarFrame(tk.Frame):
             text='My Profile',
             command=self._on_my_profile_bttn_click
         )
-        self._bttn_my_internships = tk.Button(
-            self,
-            text='My Internships',
-            command=self._on_my_internships_bttn_click
-        )
         self._bttn_search_internships = tk.Button(
             self,
             text='Search Internships',
@@ -27,19 +22,12 @@ class TopBarFrame(tk.Frame):
 
         # Pack all widgets into this frame
         self._bttn_my_profile.pack(side=tk.LEFT, anchor=tk.W, padx=3, pady=2)
-        self._bttn_my_internships.pack(side=tk.LEFT, anchor=tk.W, padx=3, pady=2)
         self._bttn_search_internships.pack(side=tk.RIGHT, anchor=tk.E, padx=3, pady=2)
     
     def _on_my_profile_bttn_click(self):
         """Responds to the event when the "My Profile" button is clicked."""
         print('"My Profile" was clicked')
         self.master.view_profile()
-
-    def _on_my_internships_bttn_click(self):
-        """Responds to the event when the "My Internships"
-        button is clicked.
-        """
-        print('"My Internships" was clicked')
 
     def _on_search_internships_bttn_click(self):
         """Responds to the event when the "Search Internships"
