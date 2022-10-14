bf7c5063b6555cc2ad8ba565ad88301153cd46c0
johndpjr <johndpowell02@gmail.com>
2022-10-14 00:24:11 -0500

    fix func name collisions and infinite recursion

--- agtern/pipelines/frontend/api.py
+++ agtern/pipelines/frontend/api.py
@@ -1,8 +1,11 @@
 """Pre-MVP: This file calls functions in agtern.backend.server to retrieve data.
 Post-MVP: This file will provide functions that will query the AgTern web server."""
 
+from typing import List
+
 from ..backend import get_all_internships
+from ...models import Internship
 
 
-def get_all_internships():
+def api_get_all_internships() -> List[Internship]:
     return get_all_internships()
