f55a233ad5705c3fe02276de2fd8d3b873f13c1a
johndpjr <johndpowell02@gmail.com>
2022-10-29 20:02:38 -0500

    add retries and session to api

--- agtern/common/api.py
+++ agtern/common/api.py
@@ -1,4 +1,5 @@
 import requests
+from requests.adapters import HTTPAdapter, Retry
 
 from .schemas import InternshipBase
 from .logger import LOG
@@ -17,12 +18,15 @@ class AgTernAPI:
         else:
             # NOTE: update this when we are cloud-hosted
             self.base_url = self.SERVER_URL
+        self.session = requests.Session()
+        retry = Retry(total=5, backoff_factor=1)
+        self.session.mount("http://", HTTPAdapter(max_retries=retry))
 
     def get_all_internships(self):
         """Retrieve all internships."""
 
         LOG.info("Retrieving internships...")
-        resp = requests.get(self.base_url + "/internships/")
+        resp = self.session.get(self.base_url + "/internships/")
         if resp.ok:
             data = resp.json()
             return [InternshipBase(**iship) for iship in data]
