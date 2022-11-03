1b87c7f979b37f128e4be7a3a561f2f90fa891fa
Jeremy E <34139712+JeremyEastham@users.noreply.github.com>
2022-11-02 21:56:12 -0500

    Fix __init__.py

--- agtern/common/__init__.py
+++ agtern/common/__init__.py
@@ -1,4 +1,4 @@
-from .models import Internship
+from .models import InternshipBaseModel
 from .data import DataFile, DataFolder
 from .api import AgTernAPI
 from .schemas import Internship, InternshipCreate
