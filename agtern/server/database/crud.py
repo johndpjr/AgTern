873db366838478727d7cb977eb4e4460b8d846cd
Jeremy E <34139712+JeremyEastham@users.noreply.github.com>
2022-11-04 15:23:44 -0500

    Add internship conversion logic

--- agtern/server/database/crud.py
+++ agtern/server/database/crud.py
@@ -1,3 +1,5 @@
+from typing import List, Tuple
+
 from pydantic import ValidationError
 from sqlalchemy.orm import Session
 
@@ -5,31 +7,58 @@ from .models import DatabaseInternship
 from agtern.common import LOG, Internship, InternshipCreateSchema
 
 
+def convert_internships(*db_internships: DatabaseInternship) -> List[Internship]:
+    """Converts DatabaseInternships as stored in the database to an Internship that can be returned to the user"""
+    internships = []
+    for db_internship in db_internships:
+        try:
+            data = {
+                column: getattr(db_internship, column)
+                for column in Internship.__fields__.keys()
+                if column in DatabaseInternship.__table__.columns.keys()
+            }
+            internships.append(
+                Internship(**{
+                    k: v for k, v in data.items()
+                    if v is not None and len(str(v)) != 0  # Don't add Nones or empty strings
+                })
+            )
+        except ValidationError as errors:
+            LOG.error("Unable to create Internship!")
+            LOG.error(f"DatabaseInternship: {db_internship}")
+            LOG.error(errors)
+    return internships
+
+def convert_internship(internship) -> Internship:
+    ret = convert_internships(internship)
+    return ret[0]
+
 def get_internship(db: Session, internship_id: int) -> Internship:
     """Gets an internship by internship_id."""
-    return db.query(DatabaseInternship)\
-        .filter(DatabaseInternship.id == internship_id)\
-        .first()
+    return convert_internship(
+            db.query(DatabaseInternship)
+            .filter(DatabaseInternship.id == internship_id)
+            .first()
+        )
 
 def get_all_internships(db: Session, skip: int = 0, limit: int = 100) -> list[Internship]:
     """Gets all internships. Use skip and limit to offset and limit number of results."""
-    return db.query(DatabaseInternship)\
-        .offset(skip)\
-        .limit(limit)\
-        .all()
+    return convert_internships(
+            *db.query(DatabaseInternship)
+            .offset(skip)
+            .limit(limit)
+            .all()
+        )
 
 
-def create_internships(db: Session, *internships: DatabaseInternship):
+def create_internships(db: Session, *internships: DatabaseInternship) -> List[Internship]:
     if len(internships) > 0:
         db.add_all(internships)
         db.commit()
         # Maybe call db.refresh()?
-    return internships
+    return convert_internships(*internships)
 
-def create_internship(db: Session, internship: DatabaseInternship):
+def create_internship(db: Session, internship: DatabaseInternship) -> Internship:
     """Creates an internship in the database."""
     ret = create_internships(db, internship)
-    if ret is None or len(ret) == 0:
-        return None
-    else:
-        return ret[0]  # We know here that len == 1
+    return ret[0]
