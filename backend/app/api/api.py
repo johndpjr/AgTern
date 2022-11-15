0161312815b32f54599d5c4ffee63573ef19a596
John Powell <johndpowell02@gmail.com>
2022-11-15 07:35:03 -0600

    Restructure project (#79)
    * restructure project
    
    * make /internships routes relative
    
    * fix circular import error
    
    * Reformat for pre-commit
    
    * Fix more circular dependencies
    
    Co-authored-by: brendanehrman1 <brendanehrman@yahoo.com>

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -3,4 +3,6 @@ from fastapi import APIRouter
 from .endpoints import internships
 
 api_router = APIRouter()
-api_router.include_router(internships, prefix="/internships", tags=["internships"])
+api_router.include_router(
+    internships.router, prefix="/internships", tags=["internships"]
+)
