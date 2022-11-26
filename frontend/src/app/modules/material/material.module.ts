1cfa371d89d3d63a9e1caeb74315b462eaa2576e
john <johndpowell02@gmail.com>
2022-11-25 22:16:48 -0600

    add MaterialModule to organize Material imports

---
+++ frontend/src/app/modules/material/material.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {MatButtonModule} from "@angular/material/button"
+import {MatCardModule} from "@angular/material/card"
+import {MatChipsModule} from "@angular/material/chips"
+import {MatDialogModule} from "@angular/material/dialog"
+import {MatDividerModule} from "@angular/material/divider"
+import {MatIconModule} from "@angular/material/icon"
+import {MatInputModule} from "@angular/material/input"
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatSelectModule} from "@angular/material/select"
+import {MatSidenavModule} from "@angular/material/sidenav"
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatToolbarModule} from "@angular/material/toolbar"
+
+@NgModule({
+  exports: [
+    CommonModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatIconModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+  ]
+})
+export class MaterialModule { }
