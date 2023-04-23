f7523528b6defa166f301dcfd2c277d7f5db8f17
johndpjr <johndpowell02@gmail.com>
2023-04-22 23:35:50 -0500

    show tags on job details

--- frontend/src/app/job-details/job-details.component.ts
+++ frontend/src/app/job-details/job-details.component.ts
@@ -25,16 +25,6 @@ import {animate, style, transition, trigger} from "@angular/animations";
 export class JobDetailsComponent {
   @Input() public job!: Job
 
-  tags: string[] = []
-
   ngOnInit(): void {
-    let all_tags = (
-      "cloud electrical management sales big-data " +
-      "ui web-dev finance robotics research " +
-      "ai nlp design c++ python " +
-      "database statistics seo devops security " +
-      "java c# rust"
-    ).split(" ").sort(() => 0.5 - Math.random())
-    this.tags = all_tags.slice(0, Math.floor(Math.random() * 8) + 3)
   }
 }
