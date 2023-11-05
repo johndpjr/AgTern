c16efea32445a8c532d5b3e5d0ab76434d1e43ac
Jwdegames <jacob@enerio.net>
2023-11-05 02:08:47 -0600

    Made each logger commit to their own file. This makes it easier to check which each program was doing. Also, allow logger to be run in parallel.

--- backend/app/utils/logger.py
+++ backend/app/utils/logger.py
@@ -1,28 +1,28 @@
 import logging
-import logging.handlers
-from os import makedirs
+from os import makedirs, getpid
 from pathlib import Path
-
+import time
 
 def setup_logger(name):
     # logger settings
     makedirs(Path.cwd().joinpath("log"), exist_ok=True)
-    file = "log/agtern.log"
+    file = "log/agtern.log.{}.{}".format(time.strftime("(%Y-%m-%d)-(%H-%M-%S)"), getpid())
     format = "%(asctime)s [%(levelname)s] %(message)s"
 
     # setup handlers and formatters
-    rotate_handler = logging.handlers.TimedRotatingFileHandler(
-        file, when="midnight", interval=1, backupCount=7
-    )
+    # rotate_handler = logging.handlers.TimedRotatingFileHandler(
+    #     file, when="midnight", interval=1, backupCount=7
+    # )
+    file_handler = logging.FileHandler(file)
     console_handler = logging.StreamHandler()
     log_formatter = logging.Formatter(format)
 
     # attach handlers to logger
     logger = logging.getLogger(name)
     console_handler.setFormatter(log_formatter)
-    rotate_handler.setFormatter(log_formatter)
+    file_handler.setFormatter(log_formatter)
     logger.addHandler(console_handler)
-    logger.addHandler(rotate_handler)
+    logger.addHandler(file_handler)
 
     return logger
 
