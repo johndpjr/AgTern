3c7dd4c7eab9767911959f42f3733f1413e7268d
Jeremy E <34139712+JeremyEastham@users.noreply.github.com>
2023-11-06 16:02:32 -0600

    Scrape Allstate

--- backend/scraping/companies/companies.py
+++ backend/scraping/companies/companies.py
@@ -10,6 +10,20 @@ def default_process():
     get_tags("title", "description")
 
 
+@scrape_internships("Allstate")
+def scrape_allstate():
+    scrape_text("title", "category", "location", "post_date")
+    for link in scrape_links("posting_link"):
+        goto(link)
+        scrape_text("company_job_id", "description")
+        scrape_links("apply_link")
+
+
+@process_internship("Allstate")
+def process_allstate():
+    default_process()
+
+
 @scrape_internships("Tesla")
 def scrape_tesla():
     # scroll_to_bottom()
