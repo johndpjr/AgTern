82ac2dd21b98e7978a09a832eabbe1c39d66fb23
Jwdegames <jacob@enerio.net>
2023-11-12 15:47:12 -0600

    Restored rotating file handler. Removed the other file handler

--- backend/app/utils/logger.py
+++ backend/app/utils/logger.py
@@ -1,4 +1,5 @@
 import logging
+import logging.handlers
 import time
 from os import getpid, makedirs
 from pathlib import Path
@@ -13,19 +14,18 @@ def setup_logger(name):
     format = "%(asctime)s [%(levelname)s] %(message)s"
 
     # setup handlers and formatters
-    # rotate_handler = logging.handlers.TimedRotatingFileHandler(
-    #     file, when="midnight", interval=1, backupCount=7
-    # )
-    file_handler = logging.FileHandler(file)
+    rotate_handler = logging.handlers.TimedRotatingFileHandler(
+        file, when="midnight", interval=1, backupCount=7
+    )
     console_handler = logging.StreamHandler()
     log_formatter = logging.Formatter(format)
 
     # attach handlers to logger
     logger = logging.getLogger(name)
     console_handler.setFormatter(log_formatter)
-    file_handler.setFormatter(log_formatter)
+    rotate_handler.setFormatter(log_formatter)
     logger.addHandler(console_handler)
-    logger.addHandler(file_handler)
+    logger.addHandler(rotate_handler)
 
     return logger
 
