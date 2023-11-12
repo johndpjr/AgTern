dc38b2c4b6cbaf2dfb108d686b5e0b0330aa7ab5
Jeremy E <34139712+JeremyEastham@users.noreply.github.com>
2023-11-12 16:00:45 -0600

    Add scraping code for Amazon, move regex parsing to process pipeline

--- backend/scraping/companies/companies.py
+++ backend/scraping/companies/companies.py
@@ -24,11 +24,29 @@ def process_allstate():
     default_process()
 
 
+@scrape_internships("Amazon")
+def scrape_amazon():
+    while True:
+        scrape_text("title", "location", "post_date")
+        scrape_links("posting_link")
+        if not is_clickable("next_page"):
+            break
+        click("next_page")
+    for link in column("posting_link"):
+        goto(link)
+        scrape_text("company_job_id", "description")
+        scrape_links("apply_link")
+
+
+@process_internship("Amazon")
+def process_amazon():
+    default_process()
+
+
 @scrape_internships("Tesla")
 def scrape_tesla():
     # scroll_to_bottom()
     scrape_text("title", "year", "period", "category", "location")
-    match("title", "year", "period")
     for link in scrape_links("posting_link"):
         goto(link)
         scrape_text("description", "company_job_id", "job_type")
@@ -37,4 +55,5 @@ def scrape_tesla():
 
 @process_internship("Tesla")
 def process_tesla():
+    match("title", "year", "period")
     default_process()
