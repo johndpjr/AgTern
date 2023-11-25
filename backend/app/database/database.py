39a01067f341139375729841fa3814c422b5e74c
DavidHigpen <davidhigpen@gmail.com>
2023-11-24 21:36:29 -0600

    SQLite to PostgreSQL

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,15 +1,19 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./data/sql_app.db"
-# NOTE: to move database engines, we simply need
-#   to change our URL (SQLAlchemy handles it all!)
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+url = URL.create(
+    drivername="postgresql",
+    username="agtern",
+    password="password",
+    host="localhost",
+    database="agtern"
 )
+
+engine = create_engine(url)
+
 DatabaseSession = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 DatabaseModel = declarative_base(name="DatabaseModel")
